bd3ea7d5749f169bc1bfee11b5ce772b8c5faeeb
Johannes Hackel <jo.hackel@web.de>
2013-05-03 11:18:26 +0200

    parallel clean

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #include <stdexcept>
 #include <ios>
 #include <map>
+#include <unordered_map>
 #include <algorithm>
 #include <string>
 #include <thread>
@@ -11,9 +12,12 @@
 
 using namespace std;
 
+const int processCount = 4;
 mutex ctrMutex;
 string data;
-map<string, int> wordcounts;
+unordered_map<string, int> wordcounts;
+
+string* txtParts;
 
 string readFile(const char* filename) {
     ifstream in;
@@ -23,37 +27,59 @@ string readFile(const char* filename) {
     return sstr.str();
 }
 
-void cleanData() {
+void cleanDataThread(int index) {
+    string* threadData = &txtParts[index];
     int i = 0;
-    while(i < data.length()) {
-        string strsign = data.substr(i, 2);
-        char sign = data.at(i);
-        if(sign == '\n')
-            data.replace(i, 1, " ");
-        else if(sign == ' ')
-            data.replace(i, 1, " ");
-        else if(strsign == "Ä" || strsign == "ä") {
-            data.replace(i, 1, "ä");
+    while(i < threadData->length()) {
+        string strsign = threadData->substr(i, 2);
+        char sign = threadData->at(i);
+        if(sign == '\n') {
+            threadData->replace(i, 1, " ");
+        } else if(strsign == "Ä" || strsign == "ä") {
+            threadData->replace(i, 1, "ä");
             i++;
         } else if(strsign == "Ö" || strsign == "ö") {
-            data.replace(i, 1, "ö");
+            threadData->replace(i, 1, "ö");
             i++;
         } else if(strsign == "Ü" || strsign == "ü") {
-            data.replace(i, 1, "ü");
+            threadData->replace(i, 1, "ü");
             i++;
         } else if(strsign == "ß") {
-            data.replace(i, 1, "ß");
+            threadData->replace(i, 1, "ß");
             i++;
-        } else if(sign < 65 || (sign > 90 && sign < 97) || sign > 122) { //Sonderzeichen entfernen
-            data.replace(i, 1, "");
+        } else if(sign != ' ' && (sign < 65 || (sign > 90 && sign < 97) || sign > 122)) { //Sonderzeichen entfernen
+            threadData->replace(i, 1, "");
             i--;
         } else if(sign > 64 && sign < 91) { //große Buchstaben
             string newsign;
             newsign += (char)(sign + 32); //kleine Schreibweise
-            data.replace(i, 1, newsign);
+            threadData->replace(i, 1, newsign);
         }
         i++;
     }
+}
+
+void cleanData() {
+    string tp[processCount];
+    txtParts = tp;
+    int threadlength = data.length() / processCount;
+    thread processes[processCount];
+    for(int i = 0; i < processCount; i++) {
+        int start = i * threadlength;
+        int end;
+        if((i + 1) == processCount)
+            end = data.length();
+        else
+            end = (i + 1) * threadlength;
+        txtParts[i] = data.substr(start, end - start);
+        processes[i] = thread(cleanDataThread, i);
+    }
+    data = "";
+    for(int i = 0; i < processCount; i++) {
+        processes[i].join();
+        data.append(txtParts[i]);
+    }
+    
     data.append(new char(' '));
 }
 
@@ -92,7 +118,7 @@ pair<B,A> flip_pair(const pair<A,B> &p)
 }
 
 template<typename A, typename B>
-multimap<B,A> flip_map(const map<A,B> &src)
+multimap<B,A> flip_map(const unordered_map<A,B> &src)
 {
     multimap<B,A> dst;
     transform(src.begin(), src.end(), std::inserter(dst, dst.begin()), flip_pair<A,B>);
@@ -104,7 +130,6 @@ int main(int argc, char **argv) {
         cout << "Einzulesende Datei angeben\n" << endl;
         return 0;
     }
-    int processCount = 4;
     data = readFile(argv[1]);
     //cout << data << endl;
     
@@ -130,8 +155,10 @@ int main(int argc, char **argv) {
     cout << "\nWortanzahl:\n" << endl;
     
     multimap<int, string> sortedMap = flip_map(wordcounts);
-    for(multimap<int, string>::iterator iter = --sortedMap.end(); iter!=sortedMap.begin(); --iter) {
+    for(multimap<int, string>::iterator iter = sortedMap.begin(); iter!=sortedMap.end(); ++iter) {
         cout << iter->second << ": " << iter->first << endl;
     }
+    
+    cin.get();
     return 0;
 }
